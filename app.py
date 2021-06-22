40afe76d92310fb54044840d79fdec8e40d97648
StevenBourn <stevenbourn2@hotmail.com>
2021-06-22 13:59:05 +0000

    changed to use variabl

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from os import getenv
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return f"{getenv('my_text')}"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
