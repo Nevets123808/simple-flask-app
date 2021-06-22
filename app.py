ce1a54bd3bd1d4120d1d9222d07af5c8ff1c6767
StevenBourn <stevenbourn2@hotmail.com>
2021-06-22 14:16:03 +0000

    do

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"{getenv('my_text')}"
+    return f"{getenv('my_text')}, ok?"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
